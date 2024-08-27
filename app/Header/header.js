79a408cc6a5bc6eddf1bb0bc8b312432098b63a2
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-08-26 21:02:58 -0600

    Initial commit

---
+++ app/Header/header.js
@@ -0,0 +1,11 @@
+function HeaderComponent() {    
+  return (        
+      <div>
+        
+        I am header component
+        
+        
+        </div>    
+  )
+}
+export default HeaderComponent
