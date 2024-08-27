03d298974668f14a505d93393dc932e4c5cfbcac
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-08-26 21:31:31 -0600

    second commit

--- app/Footer/footer.js
+++ app/Footer/footer.js
@@ -1,6 +1,15 @@
 function FooterComponent() {    
-	return (        
-		<div>I am Footer Component</div>    
-	)
-}
+	return (              
+            <footer className="footer">
+            {/* <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav> */}
+        </footer>  
+          )
+        }
 export default FooterComponent
--- app/Header/header.js
+++ app/Header/header.js
@@ -1,11 +1,18 @@
+
+
 function HeaderComponent() {    
   return (        
-      <div>
-        
-        I am header component
-        
-        
-        </div>    
+    <header className="header">
+    <img src="G.gif" width='100px' alt="Company Logo" className="logo" />
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>  
   )
 }
 export default HeaderComponent
