bd21d7c89696cc5d09de7df47bea73947b54d9c1
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-08-27 19:09:01 -0600

    Fixing styling on header and footer

--- app/Footer/footer.js
+++ app/Footer/footer.js
@@ -1,14 +1,7 @@
 function FooterComponent() {    
 	return (              
             <footer className="footer">
-            {/* <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav> */}
+            
         </footer>  
           )
         }
