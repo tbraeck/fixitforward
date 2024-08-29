253872f767dd3adae24f617d41e743f1cd28fcf5
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-08-29 13:40:32 -0600

    creating form

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import FooterComponent from "./Footer/footer";
 import HeaderComponent from "./Header/header";
 
@@ -6,7 +7,9 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <HeaderComponent />
       <div className="main-content flex-grow">
-        Hello world!
+        <div className="flex justify-center items-center bg-gray">
+          <Link href='/fix-it-forward'><button>Fix It Forward</button></Link>
+        </div>
       </div>
       <FooterComponent />
     </div>
